f14c8b1bfc927640215f190677b3c5235342d691
vikas <vikasgowdam022@gmail.com>
2026-03-02 22:08:25 +0530

    brightness

---
+++ opencv_practise/eleven.py
@@ -0,0 +1,10 @@
+#increase brightnes
+import cv2
+import matplotlib.pyplot as plt
+img=cv2.imread("data/image1.jpeg")
+bright_contrast=cv2.convertScaleAbs(img,alpha=1.5,beta=50)
+img_rgb=cv2.cvtColor(bright_contrast,cv2.COLOR_BGR2RGB)
+plt.imshow(img_rgb)
+plt.title("brightened image")
+plt.axis("off")
+plt.show()
