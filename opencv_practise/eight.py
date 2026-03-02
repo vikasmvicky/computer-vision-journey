8ca14e35316b9694aff030cd053786afb63d6771
vikas <vikasgowdam022@gmail.com>
2026-03-02 21:56:03 +0530

    swap red and blue

--- opencv_practise/eight.py
+++ opencv_practise/eight.py
@@ -2,7 +2,7 @@
 import cv2
 import matplotlib.pyplot as plt
 img=cv2.imread("data/image1.jpeg")
-red_channel=img[:,:,200]
-plt.imshow(red_channel,cmap="gray")
+red_channel=img[:,:,2]
+plt.imshow(red_channel)
 plt.title("red channel")
 plt.show()
---
+++ opencv_practise/nine.py
@@ -0,0 +1,11 @@
+import cv2 # swap red and blue
+import matplotlib.pyplot as plt
+img=cv2.imread("data/image1.jpeg")
+swapped=img.copy()
+swapped[:,:,0]=img[:,:,2]
+img[:,:,2]=img[:,:,0]
+swapped_rgb=cv2.cvtColor(swapped,cv2.COLOR_BGR2RGB)
+plt.imshow(swapped_rgb)
+plt.title("red-blue swapped")
+plt.axis("off")
+plt.show()
